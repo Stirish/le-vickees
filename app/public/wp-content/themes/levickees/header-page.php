b67864f2c267551a893a3bdf2212addd06338759
Stirish <floo.swartzy@hotmail.fr>
2023-03-10 20:30:48 +0100

    Terminé

--- app/public/wp-content/themes/levickees/header-page.php
+++ app/public/wp-content/themes/levickees/header-page.php
@@ -6,6 +6,13 @@
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
     <?php wp_head() ?>
+    <script>
+        var script = document.createElement('script');
+        script.dataset.cache = true;
+        script.dataset.websiteId = '4c791654-0bb6-43e2-9e92-e147793774b8';
+        script.src = 'https://s.abla.io/abla.js';
+        document.getElementsByTagName('head')[0].appendChild(script);
+    </script>
 </head>
 
 <body>
--- app/public/wp-content/themes/levickees/header.php
+++ app/public/wp-content/themes/levickees/header.php
@@ -1,9 +1,17 @@
 <!DOCTYPE html>
 <html lang='en'>
+
 <head>
     <meta charset='UTF-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+    <script>
+        var script = document.createElement('script');
+        script.dataset.cache = true;
+        script.dataset.websiteId = '4c791654-0bb6-43e2-9e92-e147793774b8';
+        script.src = 'https://s.abla.io/abla.js';
+        document.getElementsByTagName('head')[0].appendChild(script);
+    </script>
     <?php wp_head() ?>
 </head>
 
